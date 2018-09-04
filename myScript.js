325c327e0f3c1193a9a7ac713940f17920d78815
SueCavalheiro <suellen.cavalheiro@gmail.com>
2018-09-04 17:17:50 +0300

    Day 4 II - Recipe Project
    Optmization.

--- myScript.js
+++ myScript.js
@@ -1,21 +1,21 @@
 function openTab(evt, cityName) {
-  let i, tabcontent, tablinks;
+	let i, tabcontent, tablinks;
 
-  // Get all elements with class="tabcontent" and hide them
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (let i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
+	// Get all elements with class="tabcontent" and hide them
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (let i = 0; i < tabcontent.length; i++) {
+		tabcontent[i].style.display = "none";
+	}
 
-  tablinks = document.getElementsByClassName("tablinks");
-  for (let = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");    
-  }
+	tablinks = document.getElementsByClassName("tablinks");
+	for (let = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
 
-  document.getElementById(cityName).style.display = "block";
-  evt.currentTarget.className += " active";
+	document.getElementById(cityName).style.display = "block";
+	evt.currentTarget.className += " active";
 }
 
-   window.onload = function(e){
-      document.getElementById("defaultOpen").click();
-   }
+window.onload = function (e) {
+	document.getElementById("defaultOpen").click();
+}
